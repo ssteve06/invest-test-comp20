76dd69dc0e8537d41b3945d5f85d8c5ee3bcd7a1
JustinSteinberg <justin.steinberg1@aol.com>
2019-12-02 17:04:48 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+/*
+const http = require("http");
+const server = http.createServer((req, res) =>{
+	if(req.url=="/"){
+		res.write("Hello World");
+		res.end();
+	}
+	if(req.url=='/api/courses'){
+		res.write(JSON.stringify([1,2,3]));
+		res.end();
+	}
+});
+
+/*server.on('connection', (socket)=>{
+	console.log("New connection...");
+});
+
+server.listen(3000);
+
+console.log("Listening on port 3000...");
+*/
+const express = require("express");
+const fetch = require("node-fetch");
+
+const app = express();
+
+
+app.listen(3000, ()=> console.log("Listening at 3000..."));
+app.use(express.static("public"));
+
+app.get('/stock/:sym', async (req,res) => {
+	const sym = req.params.sym; 
+	console.log(sym);
+	const api_url = 'https://cloud.iexapis.com/stable/stock/'+sym+'/quote?token=pk_065b1600526c4ad5b953052a98fa7070';
+	const fetch_response = await fetch(api_url);
+	const json = await fetch_response.json();
+	res.json(json);
+});
+
