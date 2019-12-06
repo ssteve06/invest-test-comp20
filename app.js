a8d114fcaee45e513f466f49632b6e13e907856f
Max Ramer <maxramer@Maxs-MacBook-Pro-2.local>
2019-12-06 16:54:38 -0500

    started database

--- app.js
+++ app.js
@@ -5,6 +5,30 @@ const fetch = require("node-fetch");
 const app = express();
 var path = require('path');
 
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://127.0.0.1:27017'
+const dbName = 'game-of-thrones'
+let db
+
+MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+  if (err) return console.log(err)
+
+  // Storing a reference to the database so you can use it later
+  db = client.db(dbName)
+  console.log(`Connected MongoDB: ${url}`)
+  console.log(`Database: ${dbName}`)
+
+  var doc = { name: "Roshan", age: "22" };
+
+  db.collection("users").insertOne(doc, function(err, res) {
+    if (err) throw err;
+    console.log("Document inserted");
+    // close the connection to db when you are done with it
+    db.close();
+
+});
+
+
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
@@ -16,11 +40,12 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/app.html'));
 });
 
+
 app.get('/stock/:sym', async (req,res) => {
-	const sym = req.params.sym; 
+	const sym = req.params.sym;
 	console.log(sym);
 	const api_url = 'https://cloud.iexapis.com/stable/stock/'+sym+'/quote?token=pk_065b1600526c4ad5b953052a98fa7070';
-	const fetch_response = await fetch(api_url);
+	const fetch_response = await fetch(api_url)
 	const json = await fetch_response.json();
 	res.json(json);
 });
