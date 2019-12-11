9c7f7d48721747ea50b132a3e6a0ffd72db699e7
ssteve06 <48364530+ssteve06@users.noreply.github.com>
2019-12-11 16:11:45 -0500

    Update app.js

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ const dbName = 'test'
 var db
 var username;
 
-//const sgMail = require('@sendgrid/mail');
-//sgMail.setApiKey(process.env.sgMail_API_KEY);
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.sgMail_API_KEY);
 
   MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
     if (err) return console.log(err)
@@ -234,7 +234,7 @@ app.get('/signup', function(req, res) {
 });
 
 app.post('/forgotpassword', function (req, res) {
-    /*const username = req.body.username;
+    const username = req.body.username;
     var query = {"username": username};
     var doc = checkExists(db, username, query);
     doc.then(function(value) {
@@ -249,7 +249,7 @@ app.post('/forgotpassword', function (req, res) {
         };
         sgMail.send(msg);
     });
-    res.redirect(307, '/login');*/
+    res.redirect(307, '/login');
 });
 
 app.post('/newuser', function(req,res) {
